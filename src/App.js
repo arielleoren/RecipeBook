3d61d42a81b37dfc644a42d124423b34cdf866e2
arielleoren <arielleoren@gmail.com>
2020-08-17 19:32:54 +0300

    added newUser.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from './Components/NavBar';  
 import LogIn from './Components/LogIn'; 
+import NewUser from './Components/NewUser';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <LogIn />
+      {/* <LogIn /> */}
+      <NewUser />
     </div>
   );  
 }
---
+++ src/Components/NewUser.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+class NewUser extends React.Component {
+    render() {
+      return <div style={{marginTop: '100px'}}> 
+ <form>
+  <label style={{display: 'flex', justifyContent: 'center'}}>
+    name*:
+    <input type="text" name="name" required/>
+  </label>
+  <label style={{display: 'flex', justifyContent: 'center'}}>
+    username*:
+    <input type="text" name="username" required/>
+  </label>
+  <label style={{display: 'flex', justifyContent: 'center'}}>
+    email*:
+    <input type="email" name="email" required/>
+  </label>
+  <label style={{display: 'flex', justifyContent: 'center'}}>
+    password*:
+    <input type="password" name="password" required/>
+  </label>
+</form>
+<input style = {{marginBottom: '10px'}} type="submit" value="Submit" />
+</div> 
+    }
+  }
+
+  export default NewUser; 
+
