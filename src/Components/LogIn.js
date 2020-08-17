1941b4627c06f1cd45eb2753c9a154e99ccb4d9d
arielleoren <arielleoren@gmail.com>
2020-08-17 19:33:01 +0300

    added Login.js

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -4,24 +4,21 @@ import React from 'react';
 class LogIn extends React.Component {
     render() {
       return <div style={{marginTop: '100px'}}> 
- <form>
-  <label>
+ <form >
+  <label style={{display: 'flex', justifyContent: 'center'}}>
     UserName:
     <input type="text" name="username" />
   </label>
-</form>
-
-<form>
   <label>
     password:
     <input type="password" name="password" />
   </label>
 </form>
-<div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', marginTop: '30px', paddingLeft: '600px', paddingRight: '600px'}}> 
+<div style={{flexDirection: 'column', marginTop: '30px', paddingLeft: '600px', paddingRight: '600px', display: 'flex', justifyContent: 'center'}}> 
 <input style = {{marginBottom: '10px'}} type="submit" value="Submit" />
 <button> New User Click Here </button> 
-</div> 
-    </div>
+</div>
+</div>  
     }
   }
 
