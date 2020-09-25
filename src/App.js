2ae0a8c9d336645e9e0009c182da95a78ef8ea15
arielleoren <arielleoren@gmail.com>
2020-09-25 17:02:38 +0300

    added browserrouter

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import NavBar from './Components/NavBar';
 import LogIn from './Components/LogIn'; 
 import NewUser from './Components/NewUser';
 import NewRecipe from './Components/NewRecipe';
+import Childd from './Components/Childd.jsx';
+import Home from './Components/Home.jsx';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <LogIn />
-      {/* <NewUser /> */}
-      {/* <NewRecipe />  */}
+      <Router>
+        <NavBar />
+        <Route exact path = "/" component={LogIn}/>
+        <Route exact path = "/home" component={Home}/>
+        <Route exact path = "/new-user" component={NewUser}/>
+        <Route exact path = "/new-recipe" component={NewRecipe}/>
+        <Route exact path = "/childd" component={Childd}/>
+      </Router>
     </div>
   );  
 }
