be7ec896ece8eeb7fdf545b5706baac05c3f1a45
arielleoren <arielleoren@gmail.com>
2020-09-25 17:03:43 +0300

    added navlinks

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -1,27 +1,29 @@
 import React from 'react';
+import { Link, NavLink } from "react-router-dom";
 
 class LogIn extends React.Component {
+    
     render() {
-      return <div style={{marginTop: '100px'}}> 
- <form >
-  <label style={{display: 'flex', justifyContent: 'center'}}>
-    UserName:
+        return <div style={{ marginTop: '100px' }}>
+            <form >
+                <label style={{ display: 'flex', justifyContent: 'center' }}>
+                    UserName:
     <input type="text" name="username" />
-  </label>
-  <label>
-    password:
+                </label>
+                <label>
+                    password:
     <input type="password" name="password" />
-  </label>
-</form>
-<div style={{flexDirection: 'column', marginTop: '30px', paddingLeft: '600px', paddingRight: '600px', display: 'flex', justifyContent: 'center'}}> 
-<input style = {{marginBottom: '10px'}} type="submit" value="Submit" />
-<button> New User Click Here </button> 
-{/* need to figure out how to redirect */}
-</div>
-</div>  
-    }
-  }
+                </label>
+            </form>
+            <div style={{ flexDirection: 'column', marginTop: '30px', paddingLeft: '600px', paddingRight: '600px', display: 'flex', justifyContent: 'center' }}>
+                <input style={{ marginBottom: '10px' }} type="submit" value="Submit" />
+                <button><Link to='/new-user'> New User Click Here </Link> </button>
 
-  export default LogIn;
+            </div>
+        </div>
+    }
+}
+
+export default LogIn;
 
 
