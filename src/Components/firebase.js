56d252800a1d496271d0f219804d84139a59e569
arielleoren <arielleoren@gmail.com>
2020-10-02 20:29:07 +0300

    created firebase.js file with firebase code

---
+++ src/Components/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+const config = {
+    apiKey: "AIzaSyDFpxLQex8e5w-nsUBLnZpmo_6gU-kPdq4",
+    authDomain: "recipebook-b11cd.firebaseapp.com",
+    databaseURL: "https://recipebook-b11cd.firebaseio.com",
+    projectId: "recipebook-b11cd",
+    storageBucket: "recipebook-b11cd.appspot.com",
+    messagingSenderId: "898993329511",
+    appId: "1:898993329511:web:c15c123607e34cd1ec4bc2",
+    measurementId: "G-0Y45G91XQJ"
+  };
+firebase.initializeApp(config);
+export default firebase;
