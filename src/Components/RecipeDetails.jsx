1332a33e1cf5f366cdc7d41507d018a08cb03821
arielleoren <arielleoren@gmail.com>
2020-10-08 21:47:31 +0300

    created recipe details component

---
+++ src/Components/RecipeDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import firebase from './firebase'; // <--- add this line
+
+class RecipeDetails extends React.Component {
+    render () {
+    return (
+        <div> 
+        Recipe details 
+        </div> 
+    )
+    }
+}
+
+export default RecipeDetails; 
