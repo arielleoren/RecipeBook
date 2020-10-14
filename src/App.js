a9f56f5d2869016ab57f349ef50479969bd107c8
arielleoren <arielleoren@gmail.com>
2020-10-14 20:23:11 +0300

    commented out child component link

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import NavBar from './Components/NavBar';  
 import LogIn from './Components/LogIn'; 
 import NewUser from './Components/NewUser';
+import Login from './Components/LogIn'; 
 import NewRecipe from './Components/NewRecipe';
 import Childd from './Components/Childd.jsx';
 import Home from './Components/Home.jsx';
 import AllRecipes from './Components/AllRecipes.jsx'; 
 import RecipeDetails from './Components/RecipeDetails.jsx'; 
+import EditRecipe from './Components/EditRecipe.jsx';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
@@ -18,10 +20,12 @@ function App() {
         <Route exact path = "/" component={LogIn}/>
         <Route exact path = "/home" component={Home}/>
         <Route exact path = "/new-user" component={NewUser}/>
+        <Route exact path = "/login" component = {Login} /> 
         <Route exact path = "/new-recipe" component={NewRecipe}/>
-        <Route exact path = "/childd" component={Childd}/>
+        {/* <Route exact path = "/childd" component={Childd}/> */}
         <Route exact path = "/all-recipes" component={AllRecipes}/>
-        <Route exact path ="/recipe-details/${itemId}" component={RecipeDetails}/>
+        {/* <Route exact path ="/recipe-details/${itemId}" component={RecipeDetails}/> */}
+        <Route exact path = "/edit-recipe" component={EditRecipe} />
         <Route exact path ="/recipe-details" component={RecipeDetails}/>
       </Router>
     </div>
