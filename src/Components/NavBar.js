d77589e92fe95b5c60e9c061cdf95721f9dd2fad
arielleoren <arielleoren@gmail.com>
2020-10-14 20:27:42 +0300

    commented out childd section of nav bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,19 +4,20 @@ import { Link, NavLink } from "react-router-dom";
 
 function NavBar() {
     return ( 
-        <div style={{height: '100px', border: '2px solid black'}}> 
+    <div style={{height: '100px', border: '2px solid black'}}> 
     <ul style={{listStyle: 'none', display: 'flex'}}> 
     <li style={{}}> RecipeBook </li>
     <div style={{marginLeft: '800px', display: 'flex', flexDirection: 'row'}}>  
     <li><NavLink to='/home' className="noUnderlineLink" activeClassName="activeBold">Home</NavLink> </li> 
     <li style={{marginLeft: '30px'}}><NavLink to='/all-recipes' className="noUnderlineLink" activeClassName="activeBold">All Recipes</NavLink></li> 
     <li style={{marginLeft: '30px'}}><NavLink to='/new-recipe' className="noUnderlineLink" activeClassName="activeBold">New Recipe</NavLink> </li> 
-    <li style={{marginLeft: '30px'}}><NavLink to='/childd' className="noUnderlineLink" activeClassName="activeBold">Childd</NavLink> </li> 
+    {/* <li style={{marginLeft: '30px'}}><NavLink to='/childd' className="noUnderlineLink" activeClassName="activeBold">Childd</NavLink> </li>  */}
     </div> 
     </ul> 
-    <div class="line"></div>
     </div> 
     )
 }; 
+
+
 
 export default NavBar;
