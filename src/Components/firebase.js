89bbee9423f8bdbc921bf2c3469aebcbb20a567e
arielleoren <arielleoren@gmail.com>
2020-10-14 20:50:30 +0300

    imported firebase/auth

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
+import "firebase/auth";
+
 const config = {
     apiKey: "AIzaSyDFpxLQex8e5w-nsUBLnZpmo_6gU-kPdq4",
     authDomain: "recipebook-b11cd.firebaseapp.com",
