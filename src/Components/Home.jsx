e1fce5dd5d767c9d8304fd375c47b6af8030faa8
arielleoren <arielleoren@gmail.com>
2020-10-14 21:01:37 +0300

    deleted childd from home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,19 +1,13 @@
 import React, { useState } from 'react';
-// import Childd from './Childd'
+
 
 const Home = () => {
-//   const [count, setCount] = useState(0);
 
   return (
 <div>     Recipe Book
 </div>
 
-    // <div>
-      // {/* <button onClick={() => setCount(count + 1)}>
-    //     Click me
-    //   </button>
-    //   <Childd count={count}/>
-    // </div> }
+
   )
 }
 
