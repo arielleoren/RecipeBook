7a12ed5d76bb3b5f4febd33a65cde75c5622f5f5
arielleoren <arielleoren@gmail.com>
2020-10-15 22:06:03 +0300

    added Auth.js component

---
+++ src/Components/Auth.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import firebase from './firebase'; // <--- add this line
+
+// export const AuthContext = React.createContext();
+
+// export const AuthProvider = ({ children }) 
