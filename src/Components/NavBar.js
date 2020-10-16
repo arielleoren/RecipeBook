aa2b0ff03a436675d4969fffa7b9572234cfba94
arielleoren <arielleoren@gmail.com>
2020-10-16 16:56:01 +0300

    added logout button but commented it out

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import './NavBar.css';
 import { Link, NavLink } from "react-router-dom";
+import firebase from './firebase';
 
-function NavBar() {
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+        this.logout = this.logout.bind(this); 
+    }
+
+    logout() {
+        firebase.auth().signOut()
+    }
+    render() {
     return ( 
     <div id="navbarId" style={{height: '100px', border: '2px solid black'}}> 
     <ul style={{listStyle: 'none', display: 'flex'}}> 
     <li style={{}}> RecipeBook </li>
     <div style={{marginLeft: '800px', display: 'flex', flexDirection: 'row'}}>
-    <li> <NavLink to='/home' className="noUnderlineLink" activeClassName="activeBold">Home</NavLink> </li> 
-    {/* <li style={{marginLeft: '30px'}}><NavLink to='/all-recipes' className="noUnderlineLink" activeClassName="activeBold">All Recipes</NavLink></li>  */}
-    <li style={{marginLeft: '30px'}}><NavLink to='/new-recipe' className="noUnderlineLink" activeClassName="activeBold">New Recipe</NavLink> </li> 
+    {/* <li> <NavLink to='/home' className="noUnderlineLink" activeClassName="activeBold">Home</NavLink> </li>  */}
+    <li style={{marginLeft: '30px'}}><NavLink to='/all-recipes' className="noUnderlineLink" activeClassName="activeBold">All Recipes</NavLink></li> 
+    <li style={{marginLeft: '30px'}}><NavLink to='/new-recipe' className="noUnderlineLink" activeClassName="activeBold">New Recipe</NavLink> </li>
+    {/* <li style={{marginLeft: '30px'}} onClick={this.logout}><NavLink to ='/login'> Logout </NavLink> </li>  */}
     </div> 
     </ul> 
     </div> 
     )
+    }
 }; 
 
 
