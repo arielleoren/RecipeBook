d2dca71d20c3527ad326b741b44c845c194cb471
arielleoren <arielleoren@gmail.com>
2020-10-16 16:58:09 +0300

    commented out home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import firebase from './firebase';
+// import React from 'react';
+// import firebase from 'firebase';
+// import NavBar from './NavBar';  
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+// class Home extends React.Component {
+//   constructor (props) {
+//     super(props); 
+//     this.logout = this.logout.bind(this);
+//   }
 
-  render() {
-    return (
-      <div> 
-        recipe book Home
-      </div> 
-    )
-  }
-}
-export default Home;
+//   logout() {
+//     firebase.auth().signOut();
+//   }
+
+// render() {
+//   return (
+//     <div> 
+//     <h1> recipe book home </h1> 
+//         <button onClick={this.logout}> Logout </button>
+//       </div>
+//   )
+// }
+// }
+  
+
+// export default Home;
