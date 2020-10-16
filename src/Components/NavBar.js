0fffd14b30ebda2178715ede237c39de4e6b59af
arielleoren <arielleoren@gmail.com>
2020-10-16 15:11:34 +0300

    deleted link to childd from nav bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,14 +4,13 @@ import { Link, NavLink } from "react-router-dom";
 
 function NavBar() {
     return ( 
-    <div style={{height: '100px', border: '2px solid black'}}> 
+    <div id="navbarId" style={{height: '100px', border: '2px solid black'}}> 
     <ul style={{listStyle: 'none', display: 'flex'}}> 
     <li style={{}}> RecipeBook </li>
     <div style={{marginLeft: '800px', display: 'flex', flexDirection: 'row'}}>
     <li> <NavLink to='/home' className="noUnderlineLink" activeClassName="activeBold">Home</NavLink> </li> 
-    <li style={{marginLeft: '30px'}}><NavLink to='/all-recipes' className="noUnderlineLink" activeClassName="activeBold">All Recipes</NavLink></li> 
+    {/* <li style={{marginLeft: '30px'}}><NavLink to='/all-recipes' className="noUnderlineLink" activeClassName="activeBold">All Recipes</NavLink></li>  */}
     <li style={{marginLeft: '30px'}}><NavLink to='/new-recipe' className="noUnderlineLink" activeClassName="activeBold">New Recipe</NavLink> </li> 
-    <li style={{marginLeft: '30px'}}><NavLink to='/childd' className="noUnderlineLink" activeClassName="activeBold">Childd</NavLink> </li> 
     </div> 
     </ul> 
     </div> 
