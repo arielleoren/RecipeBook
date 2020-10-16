5b15726c83cf6df6d649ab82e8e28ca9957cecc8
arielleoren <arielleoren@gmail.com>
2020-10-16 15:11:46 +0300

    delted unused auth file

--- src/Components/Auth.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import firebase from './firebase'; // <--- add this line
-
-// export const AuthContext = React.createContext();
-
-// export const AuthProvider = ({ children }) 
