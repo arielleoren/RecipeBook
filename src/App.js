e0aa2b8a613590379127696cc59a46718ffa5a85
arielleoren <arielleoren@gmail.com>
2020-10-16 16:58:00 +0300

    trying to add login button but commented it out

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './App.css';
-import NavBar from './Components/NavBar';  
-import NewUser from './Components/NewUser';
+import firebase from './Components/firebase';
+// import Home from './Components/Home.jsx';
 import Login from './Components/Login'; 
+import NavBar from './Components/NavBar';  
+// import NewUser from './Components/NewUser';
 import NewRecipe from './Components/NewRecipe';
-import Home from './Components/Home.jsx';
 import AllRecipes from './Components/AllRecipes.jsx'; 
 import RecipeDetails from './Components/RecipeDetails.jsx'; 
 import EditRecipe from './Components/EditRecipe.jsx';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import firebase from './Components/firebase';
+import { BrowserRouter as Router, Route, Redirect,} from "react-router-dom";
 
-class App extends React.Component() {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user:{},
+      user: {}, 
     }
+    this.authListener = this.authListener.bind(this); 
+    // this.checkUser = this.checkUser.bind(this)
   }
 
   componentDidMount(){
@@ -26,31 +28,43 @@ class App extends React.Component() {
   authListener() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        this.setState({user}); 
+        this.setState({ user });
       } else {
-        this.setState({user: null})
+        this.setState({ user: null }); 
       }
-    }); 
+    })
   }
+
+
+// checkUser() {
+//   if (this.state.user = "" ) //check condition
+//   {
+//     let redirectToUrl = <Redirect to={<Login />}/>
+//   }
+// }; 
 
 render() {
   return (
-    <div>
-      {this.state.user ? (<Home />) : (<Login/>)};
-      <Router>
-        <NavBar />
-        <Route exact path = "/login" component={Login}/>
-        <Route exact path = "/home" component={Home}/>
-        <Route exact path = "/new-user" component={NewUser}/>
+    <div> 
+        <Router>
+        <NavBar /> 
+        {/* {!(this.state.user) && <Redirect to="/login" />} */}
+        {
+                  // <Router> 
+        // <Route exact path="/login" render={() => <Redirect to="/login" />} />
+        // </Router>
+        }
+        {/* {if (!this.state.user)  (Continue) : (<Login />)} */}
+        {/* <Route exact path = "/login" component={Login}/>  */}
+        {/* <Route exact path = "/home" component={Home}/>  */}
         <Route exact path = "/new-recipe" component={NewRecipe}/>
         <Route exact path = "/all-recipes" component={AllRecipes}/>
         <Route exact path = "/edit-recipe" component={EditRecipe} />
         <Route exact path ="/recipe-details" component={RecipeDetails}/>
       </Router>
-    </div>
+    </div> 
   );  
 }
-
 }
 
 export default App;
