088848f6dcbec8b89016fc228ee0a4065eee97ca
arielleoren <arielleoren@gmail.com>
2020-10-16 14:11:16 +0300

    commented out & added back link in navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './NavBar.css'; 
+import './NavBar.css';
 import { Link, NavLink } from "react-router-dom";
 
 function NavBar() {
@@ -7,11 +7,11 @@ function NavBar() {
     <div style={{height: '100px', border: '2px solid black'}}> 
     <ul style={{listStyle: 'none', display: 'flex'}}> 
     <li style={{}}> RecipeBook </li>
-    <div style={{marginLeft: '800px', display: 'flex', flexDirection: 'row'}}>  
-    <li><NavLink to='/home' className="noUnderlineLink" activeClassName="activeBold">Home</NavLink> </li> 
+    <div style={{marginLeft: '800px', display: 'flex', flexDirection: 'row'}}>
+    <li> <NavLink to='/home' className="noUnderlineLink" activeClassName="activeBold">Home</NavLink> </li> 
     <li style={{marginLeft: '30px'}}><NavLink to='/all-recipes' className="noUnderlineLink" activeClassName="activeBold">All Recipes</NavLink></li> 
     <li style={{marginLeft: '30px'}}><NavLink to='/new-recipe' className="noUnderlineLink" activeClassName="activeBold">New Recipe</NavLink> </li> 
-    {/* <li style={{marginLeft: '30px'}}><NavLink to='/childd' className="noUnderlineLink" activeClassName="activeBold">Childd</NavLink> </li>  */}
+    <li style={{marginLeft: '30px'}}><NavLink to='/childd' className="noUnderlineLink" activeClassName="activeBold">Childd</NavLink> </li> 
     </div> 
     </ul> 
     </div> 
